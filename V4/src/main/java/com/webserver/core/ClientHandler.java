0225bb7f2efeb0bfe43a0fb8e55d0bd092b57d31
Alexander <1653660273@qq.com>
2023-12-06 20:40:23 +0800

    V4项目

--- V4/src/main/java/com/webserver/core/ClientHandler.java
+++ V4/src/main/java/com/webserver/core/ClientHandler.java
@@ -1,5 +1,6 @@
 package com.webserver.core;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
@@ -15,20 +16,9 @@ public class ClientHandler implements Runnable{
     @Override
     public void run() {
         try {
-            InputStream in = socket.getInputStream();
-
-            char pre = 'a', cur = 'a';
-            StringBuilder builder = new StringBuilder();
-            int d;
-            while((d = in.read()) != -1) //循环读入当前的字符
-            {
-                cur = (char)d;
-                if(pre == 13 && cur == 10) // 如果上一个字符不是回车（13 CR）且当前字符不是换行（10 LF），则一直执行循环
-                    break;
-                builder.append(cur);
-                pre = cur;
-            }
-            String line = builder.toString().trim(); //使用trim()是为了删除字符串最后的一个CR
+            //1 解析请求
+            //1.1 解析请求行
+            String line = readLine();
             System.out.println(line);
 
             String[] parts = line.split("\\s");
@@ -40,8 +30,33 @@ public class ClientHandler implements Runnable{
             System.out.println("uri: " + uri); //可能会出现下标越界异常，浏览器请求空
             System.out.println("protocol: " + protocol);
 
+            //1.2 解析消息头
+            while(!(line = readLine()).isEmpty()){ //.优先级比=要高
+                System.out.println(line);
+            }
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * 通过socket获取的输入流读取客户端发送过来的一行字符串
+     * @return
+     */
+    private String readLine() throws IOException { //读取一行操作
+        InputStream in = socket.getInputStream();
+        char pre = 'a', cur = 'a';
+        StringBuilder builder = new StringBuilder();
+        int d;
+        while((d = in.read()) != -1) //循环读入当前的字符
+        {
+            cur = (char)d;
+            if(pre == 13 && cur == 10) // 如果上一个字符不是回车（13 CR）且当前字符不是换行（10 LF），则一直执行循环
+                break;
+            builder.append(cur);
+            pre = cur;
+        }
+        return builder.toString().trim(); //使用trim()是为了删除字符串最后的一个CR
     }
 }
