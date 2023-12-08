faa3eb279461e4b9bf42b4230528cb585d754809
Alexander <1653660273@qq.com>
2023-12-08 20:41:16 +0800

    V6项目 创建

---
+++ V6/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,37 @@
+package com.webserver.core;
+
+import com.webserver.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 该线程任务负责与指定客户端完成HTTP交互请求
+ */
+public class ClientHandler implements Runnable{
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //1 解析请求
+            HttpServletRequest request = new HttpServletRequest(socket);
+
+            //2 处理请求
+
+            //3 发送响应
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
